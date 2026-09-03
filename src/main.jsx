b24d9b9bdf6cbb95c8d2788b9b8076eb55a44592
savivah-lab <kelvoonlineproceeds@gmail.com>
2026-09-03 11:29:42 +0300

    Wrap AdminApp in ErrorBoundary component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AdminApp from "./App.jsx";
+import { ErrorBoundary } from "./ErrorBoundary.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AdminApp />
+    <ErrorBoundary>
+      <AdminApp />
+    </ErrorBoundary>
   </React.StrictMode>
 );
